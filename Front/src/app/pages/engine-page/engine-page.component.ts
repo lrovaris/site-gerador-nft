3575f46d1d64df260e3ae85522b5ea73da3a4cc1
lrovaris <luis.gabrielrovaris@gmail.com>
2022-04-26 00:11:41 -0300

    metadata update
    Added 2 text input, Name n Decription.
    Added generate metadata button
    
    enginePageComponent:
    generate collection now only generates a collecton with indexes
    refac preview randon asset
    new method renderAssetByID
    new method generateMetadata (unfinished) (it also renders the whole collection on screen)

--- Front/src/app/pages/engine-page/engine-page.component.ts
+++ Front/src/app/pages/engine-page/engine-page.component.ts
@@ -1,6 +1,5 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
-
 // @ts-ignore
 import mergeImages from 'merge-images';
 const hash = require('object-hash');
@@ -17,19 +16,21 @@ export class EnginePageComponent implements OnInit {
   public imagePath: any;
   imgUR1L: any;
   imgUR2L: any;
-  imgUR3L: any;
+  img3URL: any;
   public message: string | undefined;
   index = 1;
-  limit = 1001
+  limit = 10
   layersToMerge:any = []
-  randomAsset: any = [];
+  randomAsset: any = {};
   hashArray: any = [];
   collectionArray:any = []
   srcIMG:string = ''
-  metadata:any = { attributes: [], src: '' };
+  metadata:any = { };
+  metadataArray:any  = [];
+  characterCount: number = 64;
+  switch = false
 
   constructor() { }
-
 
   getRandomInt(min: number, max: number) {
     min = Math.ceil(min);
@@ -41,8 +42,6 @@ export class EnginePageComponent implements OnInit {
   addCategorie(name: any) {
     this.categories.push({name: name, files: []});
   }
-
-
   uploadImage(event: any, index: any) {
     if (event.target.files && event.target.files[0] ) {
       const reader = new FileReader();
@@ -52,85 +51,115 @@ export class EnginePageComponent implements OnInit {
       reader.readAsDataURL(event.target.files[0]);
     }
   }
+        // should return an object with one index from each category  {categoryName:index}
+  generateRandonAsset() {
 
-  async getSrcImageUrl() {
-    this.metadata['src'] = await mergeImages(this.layersToMerge);
-  }
+    let randonAsset:any = {};
 
-  async setPreviewImgUrl() {
-    this.imgUR3L = await mergeImages(this.layersToMerge);
-  }
-
-  generateRandonAsset(collection:boolean) {
-
-    this.categories.forEach(categorie => {
-
-      let asset = categorie.files[this.getRandomInt(0, categorie.files.length)]
-      let assetName = asset.name.substr(0, (asset.name.length - 4))
-
-      this.randomAsset.push(
-        {asset, assetName, categorieName: categorie.name, src:''}
-      )
+    this.categories.forEach( category => {
+      randonAsset[category.name] = this.getRandomInt(0, category.files.length);
     })
 
 
+    return randonAsset;
 
-    for (let i = 0; i < this.randomAsset.length; i++) {
+
+
+ /*
+     this.randomAsset = []
+
+    this.categories.forEach(categorie => {
+      let asset = categorie.files[this.getRandomInt(0, categorie.files.length)]
+      let assetName = asset.name.substring(0, (asset.name.length - 4))
+
       const reader = new FileReader();
-      reader.readAsDataURL(this.randomAsset[i].asset)
+      reader.readAsDataURL(asset)
       reader.onload = async () => {
-        this.layersToMerge.push(reader.result);
-        this.metadata['attributes'].push({trait_type:this.randomAsset[i].categorieName, value: this.randomAsset[i].assetName})
-        if (i == (this.randomAsset.length - 1)) {
-
-         await Promise.all(
-            [this.getSrcImageUrl(), this.setPreviewImgUrl()]
-          )
-        }
+        this.randomAsset.push(
+          {asset: reader.result, assetName, categorieName: categorie.name}
+        )
       };
+
+    })
+
+  for (const layer of this.randomAsset) {
+    const i1: number = this.randomAsset.indexOf(layer);
+    this.layersToMerge.push(layer.asset)
+    console.log(i1)
+    console.log(this.randomAsset.length-1)
+    if (i1 == this.randomAsset.length - 1) {
+      this.img3URL = await mergeImages(this.layersToMerge)
+      this.metadata.src = this.img3URL;
+      console.log('teste')
+      return this.randomAsset;
     }
   }
+ */
+}
 
-  CleanCategories() {
+  // well, if you need explanation for this, you shouldn't be reading it.
+  cleanCategories() {
     this.categories = []
   }
+       // this should loop over collection length, in each loop hash an randon asset verify if its unique then add in hashedArray and collectionArray.
+  generateCollection() {
 
- async generateCollection() {
+    this.collectionArray = []
+    this.hashArray = []
+    this.index = 1;
 
-    console.log('a')
+    while(this.index <= this.limit) {
+      let randonAsset = this.generateRandonAsset();
 
-    this.randomAsset = []
+      let hashedObject = hash(randonAsset)
 
-    await this.generateRandonAsset(true);
+      let duplicate = this.hashArray.find((hash:any) => hash == hashedObject)
 
-    let hashedAsset = hash(this.randomAsset);
+      if(!duplicate) {
 
-    let findDuplicate = this.hashArray.find((asset: any) => asset == hashedAsset);
+        this.hashArray.push(hashedObject)
 
-    if(findDuplicate) {
-      await this.generateCollection()
-    } else {
-      this.collectionArray.push(this.randomAsset);
-      this.index++
+        this.collectionArray.push(randonAsset)
+
+        this.index += 1;
+
+      }
     }
 
-    if (this.index > this.limit) {
-      console.log(this.collectionArray)
-      this.index = 1
-      return
-    } else {
-      console.log(this.index);
-      await this.generateCollection()
-    }
+    console.log(this.collectionArray)
+
 
   }
-
 
   ngOnInit(): void {
 
   }
+  // this should set the img3Url to an base64 img url string from an asset inside collection chose randomly
+  generateRandonPreviewFromCollection() {
 
+    // reset the last render
+    this.img3URL = '';
+    this.layersToMerge = []
 
+    // first lets gather the randon asset to be rendered
+    let asset = this.collectionArray[this.getRandomInt(0, this.collectionArray.length)]
+
+   // then we need to get each layer file as an URL to merge with 'merge-image'
+    this.categories.forEach(categorie => {
+      const reader = new FileReader();
+      reader.readAsDataURL(categorie.files[asset[categorie.name]])
+      reader.onload = async () => {
+        this.layersToMerge.push(reader.result)
+        if(this.layersToMerge.length == this.categories.length) {
+          //then finally setting the img3URL to the merge
+          this.img3URL = await mergeImages(this.layersToMerge);
+        }
+      };
+    })
+
+  }
+
+  //TODO description
   getRandomIndexByProbability(probabilities :any) {
     let r = Math.random(),
       index = probabilities.length - 1;
@@ -146,5 +175,84 @@ export class EnginePageComponent implements OnInit {
     return index;
   }
 
+  // it wll get the id of an asset in the collection, then translating the name of each trait, then merging all layers into one base64 img and returning it
+ async renderAssetById(id:any) {
+
+
+
+    this.img3URL = '';
+    let layersToMerge: (string | ArrayBuffer | null)[] = []
+    let asset = this.collectionArray[id];
+    let oldNumber = 0
+
+    this.categories.forEach((layer:any) => {
+      oldNumber = asset[layer.name]
+      asset[layer.name] = layer.files[asset[layer.name]].name
+      const reader = new FileReader();
+      reader.readAsDataURL(layer.files[oldNumber])
+
+      reader.onload = async () => {
+        layersToMerge.push(reader.result)
+        if(layersToMerge.length == this.categories.length) {
+          asset['src'] = await mergeImages(layersToMerge);
+          return new Promise(resolve => setTimeout(() => resolve(asset), 0))
+        }
+      };
+
+    })
+
+
+
+
+}
+
+  generateMetadata(coinName: string, projectName: string, description: string) {
+
+    this.metadata = {};
+
+    if(this.collectionArray.length == 0 ) {
+      alert('you should generate a collection first')
+    }
+    if (description.length > this.characterCount) {
+      alert("description can't be larger than 64 characters")
+    }
+
+    switch (coinName) {
+
+      case 'ada':
+
+        let promisesArray: any[] = []
+
+        this.collectionArray.forEach( async (nft:any)  => {
+
+          promisesArray.push(
+            this.renderAssetById(this.collectionArray.indexOf(nft, 0))
+          )
+
+          this.metadata = {
+            ProjectName: projectName,
+            Description: description,
+            type: 'image/png',
+            src: ''
+          }
+
+          this.metadataArray.push(this.metadata)
+
+        })
+
+        console.log(promisesArray)
+
+
+        Promise.all(promisesArray).then(() => {
+          console.log('collection array??????', this.collectionArray)
+          this.switch = true;
+        })
+
+        break
+      case 'eth':
+
+        break
+    }
+  }
 
 }
